ac9605f546862f3fdeabb1a98bb2ffce069500c6
dr2pedro <dr2p@hotmail.com>
2023-02-23 19:20:58 -0300

    ...

--- src/www/utils/errors/MongoAggregateCouldNotCompleted.ts
+++
@@ -1,9 +0,0 @@
-class MongoAggregateCouldNotCompleted extends Error {
-    constructor(error: unknown) {
-        super(`The aggregation could not be completed due: ${error}`);
-    }
-}
-
-export {
-    MongoAggregateCouldNotCompleted
-}
--- src/www/utils/errors/NoGpuAvaiable.ts
+++
@@ -1,9 +0,0 @@
-class NoGpuAvaiable extends Error {
-    constructor() {
-        super('There is no GPU avaiable.')
-    }
-}
-
-export {
-    NoGpuAvaiable
-}
